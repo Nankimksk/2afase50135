e7e5a835c0acf70c5855fe943d1b1c434957d206
Nankimksk <psnartan777@gmail.com>
2024-08-21 17:19:20 -0300

    Add componentes e imagens

---
+++ componentesReact/src/components/Body.jsx
@@ -0,0 +1,14 @@
+import './Body.css'
+
+
+function Body() {
+  return (
+    <div className='body-container'>
+      <h2>Dante Sparda</h2>
+      <img className="images-body" src="./images/Dantedmc3.webp" alt="" />
+    </div>
+    
+  )
+}
+
+export default Body
---
+++ componentesReact/src/components/Vergil.jsx
@@ -0,0 +1,12 @@
+import './Vergil.css'
+
+function Vergil() {
+  return (
+    <div className='vergil-container'>
+      <h2>Vergil Sparda</h2>
+      <img className='imagens-vergil' src="./images/LiteralmenteEu.png" alt="" />
+    </div>
+  )
+}
+
+export default Vergil
